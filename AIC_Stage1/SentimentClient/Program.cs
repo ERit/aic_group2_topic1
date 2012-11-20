62b764edb8f797777e7dae7c79ea1c49807b88bb
Manu Weilharter <caitilyn@gmail.com>
2012-11-20 16:26:50 +0100

    added new solution using WCF with the implemented StatisticService

---
+++ AIC_Stage1/SentimentClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SentimentClient.StatisticServiceReference;
+
+namespace SentimentClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1: Create an instance of the WCF proxy.
+            StatisticClient client = new StatisticClient();
+
+            // Step 2: Call the service operations.
+            // Call the getStatisticValue operation
+            double result = client.getStatisticValue();
+            Console.WriteLine("Value: " + result);
+
+
+            // just for testing purpose to watch the result of the getStatisticsValue method
+            System.Threading.Thread.Sleep(10000); 
+           
+
+            //Step 3: Closing the client gracefully closes the connection and cleans up resources.
+            client.Close();
+        }
+    }
+}
---
+++ AIC_Stage1/SentimentLib/StatisticService.cs
@@ -0,0 +1,198 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using TweetSharp;
+
+namespace SentimentLib
+{
+    public class StatisticService : IStatistic
+    {
+        public double getStatisticValue()
+        {
+            Console.WriteLine("Getting the tweets and starting the sentiment analysis ... Please wait...");
+
+            return useSentiment140(getTweets("Microsoft"));
+        }
+
+        private string getTweets(string companyname)
+        {
+            // Twitter Username: 2012AIC, PW: !aicgroup
+            TwitterService twitterService = new TwitterService("H70mkcnY9uKDDGUcKywlBA", "fiobr6kG9OKwNHIU5D18dbpxWE5KdxWD8GRPRhMVII");
+            OAuthAccessToken access = twitterService.GetAccessTokenWithXAuth("2012AIC", "!aicgroup");
+
+            TwitterSearchResult response = twitterService.Search(companyname, 10, 100);
+
+            JObject newapiresults = null;
+            newapiresults = JObject.Parse(response.RawSource);
+            JArray tweets = new JArray();
+            foreach (JObject status in tweets)
+            {
+                Console.WriteLine(status["text"]);
+            }
+
+            JsonSerializer serializer = new JsonSerializer();
+            RootObject rootObject = (RootObject)serializer.Deserialize(new JsonTextReader(new StringReader(response.RawSource)), typeof(RootObject));
+
+            string result = "";
+
+            result += "{'data': [";
+
+            foreach (var tweet in rootObject.results)
+            {
+                if (tweet.iso_language_code.Equals("en"))
+                {
+                    string tweettext = tweet.text;
+                    tweettext = tweettext.Replace("\"", "");
+                    tweettext = tweettext.Replace("\'", "");
+                    tweettext = tweettext.Replace("\n", " ");
+
+                    result += "{'text': '" + tweettext + "', 'query': '" + companyname + "'}, ";
+                }
+                    
+            }
+
+            // get rid of the last comma
+            result = result.Substring(0, result.Length-2);
+
+            result += "]}" ;
+
+            return result;
+        }
+
+
+
+
+
+        private double useSentiment140(string tweets)
+        {
+            // using http://www.sentiment140.com/ for sentiment analysis	
+            HttpWebRequest httpWReq = (HttpWebRequest)WebRequest.Create("http://www.sentiment140.com/api/bulkClassifyJson");
+
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] data = encoding.GetBytes(tweets);
+
+            httpWReq.Method = "POST";
+            httpWReq.ContentType = "application/x-www-form-urlencoded";
+            httpWReq.ContentLength = data.Length;
+            Stream newStream;
+
+            // sending the html post request to the sentiment140 server
+            using (newStream = httpWReq.GetRequestStream())
+            {
+                newStream.Write(data, 0, data.Length);
+            }
+
+            string result = "";
+
+            // get the response  
+            using (HttpWebResponse response = httpWReq.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                result = reader.ReadToEnd();
+            }
+
+            newStream.Close();
+
+            return get_percent(result);
+        }
+
+
+        /* 
+		 * Polarity values are:
+		 * 0: negative
+		 * 2: neutral
+		 * 4: positive
+		*/
+        private double get_percent(string result)
+        {
+            Console.WriteLine(result);
+
+            string[] split = result.Split(',');
+
+            int positive = 0;
+            int negative = 0;
+
+            foreach (string s in split)
+            {
+
+                // get polarity values
+                if (s.Contains("polarity"))
+                {
+                    if (Convert.ToInt32(s.Split(':')[1]) == 0)
+                        negative++;
+                    else
+                        positive++;
+                }
+            }
+            Console.WriteLine("Positive tweets: " + positive);
+            Console.WriteLine("Negative tweets: " + negative);
+            int all = positive + negative;
+
+            // calculate percentage positive			
+            return (double)positive / (double)all * 100;
+        }	
+
+    }
+
+
+    public class Url
+    {
+        public string url { get; set; }
+        public string expanded_url { get; set; }
+        public string display_url { get; set; }
+        public int[] indices { get; set; }
+    }
+
+    public class Entities
+    {
+        public Url[] urls { get; set; }
+    }
+
+    public class Metadata
+    {
+        public int recent_retweets { get; set; }
+        public string result_type { get; set; }
+    }
+
+    public class Result
+    {
+        public string created_at { get; set; }
+        public Entities entities { get; set; }
+        public string from_user { get; set; }
+        public int from_user_id { get; set; }
+        public string from_user_id_str { get; set; }
+        public object geo { get; set; }
+        public object id { get; set; }
+        public string id_str { get; set; }
+        public string iso_language_code { get; set; }
+        public Metadata metadata { get; set; }
+        public string profile_image_url { get; set; }
+        public string source { get; set; }
+        public string text { get; set; }
+        public object to_user_id { get; set; }
+        public object to_user_id_str { get; set; }
+    }
+
+    public class RootObject
+    {
+        public double completed_in { get; set; }
+        public long max_id { get; set; }
+        public string max_id_str { get; set; }
+        public string next_page { get; set; }
+        public int page { get; set; }
+        public string query { get; set; }
+        public string refresh_url { get; set; }
+        public Result[] results { get; set; }
+        public int results_per_page { get; set; }
+        public int since_id { get; set; }
+        public string since_id_str { get; set; }
+    }
+}
