8ef33b933026871bfdb1e736a0da377d4a791e27
Manu Weilharter <caitilyn@gmail.com>
2012-11-27 19:18:04 +0100

    sql connection check

--- AIC_Stage1/SentimentClient/Program.cs
+++ AIC_Stage1/SentimentClient/Program.cs
@@ -8,17 +8,25 @@ using SentimentClient.AuthServiceReference;
 //using SentimentClient.BillingServiceReference;
 using System.Text.RegularExpressions;
 using System.ServiceModel;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 
 namespace SentimentClient
 {
     class Program
     {
+        private static StatisticClient stat_client;
+        private static AuthenticatorClient auth_client;
+
         static void Main(string[] args)
         {
-            //Step 1: Create an instance of the WCF proxy.
-            StatisticClient client = new StatisticClient();
-            AuthenticatorClient auth = new AuthenticatorClient();
+            //Step 1: Create instances of the WCF proxy.
+            stat_client = new StatisticClient();
+            auth_client = new AuthenticatorClient();
+
+            if (!checkSQLConnection())
+                errorHandler("Connection to MySql Database could not be established!");
 
             while (true)
             {
@@ -45,7 +53,7 @@ namespace SentimentClient
                         Console.Write("Passwort: ");
                         string password = Orb.App.Console.ReadPassword('*');
 
-                        if (auth.validateLogin(username, password))
+                        if (auth_client.validateLogin(username, password))
                         {
                             while (true)
                             {
@@ -61,10 +69,10 @@ namespace SentimentClient
 
                                 if (secondInput.Equals("1"))
                                 {
-                                    double result = client.getStatisticValue(auth.getCompanyFromUsername(username));
+                                    double result = stat_client.getStatisticValue(auth_client.getCompanyFromUsername(username));
                                     Console.WriteLine("Stentiment Value is beeing calculated, please wait...");
                                     Console.WriteLine();
-                                    Console.WriteLine("Sentiment Analysis for " + auth.getCompanyFromUsername(username) +
+                                    Console.WriteLine("Sentiment Analysis for " + auth_client.getCompanyFromUsername(username) +
                                         ": " + String.Format("{0:0.##}", result));
 
                                     Console.ReadLine();
@@ -132,7 +140,7 @@ namespace SentimentClient
 
                                     if (unregisterInput.Equals("yes"))
                                     {
-                                        auth.unregister(username);
+                                        auth_client.unregister(username);
                                         Console.WriteLine("Good bye!");
                                         Console.ReadLine();
                                         break;
@@ -203,7 +211,7 @@ namespace SentimentClient
                             email.Length > 0 &&
                             creditcard.Length > 0 &&
                             company.Length > 0 &&
-                            auth.register(name, passwd, firstname, lastname, email, creditcard, company))
+                            auth_client.register(name, passwd, firstname, lastname, email, creditcard, company))
                         {
                             Console.WriteLine("Thank you for your registration! Please log in.");
                             Console.ReadLine();
@@ -223,14 +231,52 @@ namespace SentimentClient
                 else if (firstInput.Equals("3"))
                 {
                     Console.WriteLine("Good bye!");
-                    Console.WriteLine("Press <ENTER> to terminate service.");
+                    Console.WriteLine("Press <ENTER> to terminate the client.");
                     Console.ReadLine();
                     break;
                 }
             }
 
-            client.Close();
-            auth.Close();
+            stat_client.Close();
+            auth_client.Close();
+        }
+
+
+        // not a "nice" solution - but working for the time beeing - feel free to improve
+        private static bool checkSQLConnection()
+        {
+            string myConnectionString = "SERVER=localhost;" +
+				"DATABASE=aic_group2_topic1;" +
+					"UID=root;" +
+					"PASSWORD=root;";
+
+            MySqlConnection connection = new MySqlConnection(myConnectionString);
+            try
+            {
+                connection.Open();
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private static void errorHandler(string errormsg)
+        {
+            Console.WriteLine(errormsg);
+            Console.WriteLine("Press <ENTER> to terminate the client.");
+            Console.ReadLine();
+            stat_client.Close();
+            auth_client.Close();
+            Environment.Exit(2);
         }
     }
 }
